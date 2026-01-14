dddce8c6a831183061a702438cba19d4d56dcfa4
Jonzzu <jonne.karttunen@gmail.com>
2026-01-15 00:22:38 +0200

    1. kysyy nimesi ja syöttää sen 2. kysyy ympyrän säteen ja tulostaa pinta-alan 3. kysyy suorakulmion kannan ja korkeuden ja tulostaa pinta-alan ja piirin 4. Kysyy 3 kokonaislukua ja tulostaa summan, tulon ja keskiarvon

--- moduuli-1/1.py
+++ moduuli-1/1.py
@@ -1,14 +1,2 @@
-fahrenheit_str =input("Anna lämpötila Fahrenheit-asteina:  ")
-fahrenheit =float(fahrenheit_str)
-celsius = (fahrenheit-32)*5/9
-print(f"Lämpötila Celsius-asteina: {celsius}")
-import math
-
-print(f"{'Pii':12s}:{math.pi:10.5f}")
-print(f"{'Neperin luku':12s}:{math.e:10.5f}")
-
-
-
-
-käyttäjä = input('Anna nimesi: ')
-print("Hauska tavata, " + käyttäjä + "!")
+kayttaja = input('Anna nimesi: ')
+print("Terve, " + kayttaja + "!")
---
+++ moduuli-1/4.py
@@ -0,0 +1,20 @@
+import math
+kokonaisluku_1 = input('Anna kokonaisluku 1: ')
+kokonaisluku_2 = input('Anna kokonaisluku 2: ')
+kokonaisluku_3 = input('Anna kokonaisluku 3: ')
+
+kokonaisluku3 = float(kokonaisluku_3)
+kokonaisluku2 = float(kokonaisluku_2)
+kokonaisluku1 = float(kokonaisluku_1)
+summa = kokonaisluku1 + kokonaisluku2 + kokonaisluku3
+tulo = kokonaisluku1 * kokonaisluku2 * kokonaisluku3
+
+print('Lukujen summa: ')
+print(summa)
+print('Lukujen tulo: ')
+print(tulo)
+print('Lukujen keskiarvo: ')
+print(summa/3)
+
+
+
