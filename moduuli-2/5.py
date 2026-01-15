c446e5a6417a5ed39c55a72f30134c8289eee3ed
Jonzzu <jonne.karttunen@gmail.com>
2026-01-15 14:49:20 +0200

    1. kysyy nimesi ja syöttää sen 2. kysyy ympyrän säteen ja tulostaa pinta-alan 3. kysyy suorakulmion kannan ja korkeuden ja tulostaa pinta-alan ja piirin 4. Kysyy 3 kokonaislukua ja tulostaa summan, tulon ja keskiarvon 5. massanmuunnos kilogrammoiksi ja grammoiksi

--- moduuli-2/5.py
+++ moduuli-2/5.py
@@ -2,12 +2,13 @@ talents = float(input('Enter talents:'))
 pounds = float(input('Enter pounds:'))
 lots = float(input('Enter lots:'))
 
-lots_to_grams = lots * 13,3
-pounds_to_lots = pounds * 32
-talents_to_pounds = talents * 20
+grams_per_lot = 13.3
+lots_per_pound = 32
+pounds_per_talent = 20
 
-total_grams = talents_to_pounds + pounds_to_lots + lots_to_grams
-kilograms = total_grams / 1000
-remaining_grams =
+total_grams = talents * pounds_per_talent * lots_per_pound * grams_per_lot + pounds * lots_per_pound * grams_per_lot + lots * grams_per_lot
+kilograms = int(total_grams // 1000)
+remaining_grams = total_grams - kilograms * 1000
+
 print('The weight in modern units:')
-print(f'{kilograms} kilograms and {total_grams} grams.')
+print(f'{kilograms} kilograms and {remaining_grams:.2f} grams.')
