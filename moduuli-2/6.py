ce81bf5413cfa500c4337d033775ba5a8b31569b
Jonzzu <jonne.karttunen@gmail.com>
2026-01-15 15:19:49 +0200

    1. kysyy nimesi ja syöttää sen 2. kysyy ympyrän säteen ja tulostaa pinta-alan 3. kysyy suorakulmion kannan ja korkeuden ja tulostaa pinta-alan ja piirin 4. Kysyy 3 kokonaislukua ja tulostaa summan, tulon ja keskiarvon 5. massanmuunnos kilogrammoiksi ja grammoiksi 6.arpoo 3 ja 4 numeroiset koodit

---
+++ moduuli-2/6.py
@@ -0,0 +1,12 @@
+import random
+
+random.randint(1:int,10:int)import random
+
+code3 = str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
+code4 = str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6))+ str(random.randint(1,6))
+
+int(code3)
+int(code4)
+
+print('3-digit code:',code3)
+print('4-digit code:',code4)
