997858ca3c6fc3f20333867db3f52e53987f389b
Jonzzu <jonne.karttunen@gmail.com>
2026-03-26 16:19:58 +0200

    koodailua

---
+++ moduuli-12/1.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+pyyntö = "https://api.chucknorris.io/jokes/random"
+
+try:
+    vastaus = requests.get(pyyntö)
+    if vastaus.status_code == 200:
+        json_vastaus = vastaus.json()
+        print(json_vastaus["value"])
+    else:
+        print(f'Virhe: HTTP-statuskoodi: {vastaus.status_code}')
+
+except requests.exceptions.RequestException as e:
+    print ("Hakua ei voitu suorittaa.")
