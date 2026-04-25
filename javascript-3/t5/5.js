c0f9175f6e12b85443a2719c08c852d6a2d3b4e0
Jonzzu <jonne.karttunen@gmail.com>
2026-04-25 22:07:03 +0300

    javascript tehtäviä

--- javascript-3/t5/5.js
+++ javascript-3/t5/5.js
@@ -93,3 +93,32 @@ const picArray = [
 ];
 
 // add your code here
+const section = document.getElementById('pictures');
+
+for (let i = 0; i < picArray.length; i++) {
+  const article = document.createElement('article');
+  article.className = 'card';
+
+  const h2 = document.createElement('h2');
+  h2.textContent = picArray[i].title;
+
+  const figure = document.createElement('figure');
+
+  const img = document.createElement('img');
+  img.src = picArray[i].image.medium;
+  img.alt = picArray[i].title;
+
+  const figcaption = document.createElement('figcaption');
+  figcaption.textContent = picArray[i].caption;
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+
+  const p = document.createElement('p');
+  p.textContent = picArray[i].description;
+
+  article.appendChild(h2);
+  article.appendChild(figure);
+  article.appendChild(p);
+
+  section.appendChild(article);
+}
